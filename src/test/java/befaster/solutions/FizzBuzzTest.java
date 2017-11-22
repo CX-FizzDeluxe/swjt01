303dd7fa0d59aff1652678868b34b74aada40d90
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-22 20:37:00 +0000

    Wed Nov 22 20:37:00 GMT 2017

--- src/test/java/befaster/solutions/FizzBuzzTest.java
+++ src/test/java/befaster/solutions/FizzBuzzTest.java
@@ -1,5 +1,30 @@
+package befaster.solutions;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class FizzBuzzTest {
+
+    @Test
+    public void testMultiple3() {
+        assertEquals("fizz", FizzBuzz.fizzBuzz(3));
+        assertEquals("fizz", FizzBuzz.fizzBuzz(6));
+        assertEquals("fizz", FizzBuzz.fizzBuzz(9));
+    }
+
+    @Test
+    public void testMultiple5() {
+        assertEquals("buzz", FizzBuzz.fizzBuzz(5));
+        assertEquals("buzz", FizzBuzz.fizzBuzz(10));
+        assertEquals("buzz", FizzBuzz.fizzBuzz(20));
+    }
+
+    @Test
+    public void testMultiple3And5() {
+        assertEquals("fizz buzz", FizzBuzz.fizzBuzz(15));
+        assertEquals("fizz buzz", FizzBuzz.fizzBuzz(30));
+        assertEquals("fizz buzz", FizzBuzz.fizzBuzz(45));
+    }
 
 }
