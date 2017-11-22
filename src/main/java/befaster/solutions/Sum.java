1dbfde14d84c69f3194ba8286662739f4fbc68b0
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-22 20:01:21 +0000

    Wed Nov 22 20:01:21 GMT 2017

--- src/main/java/befaster/solutions/Sum.java
+++ src/main/java/befaster/solutions/Sum.java
@@ -5,7 +5,12 @@ import befaster.runner.SolutionNotImplementedException;
 public class Sum {
 
     public static int sum(int x, int y) {
-        throw new SolutionNotImplementedException();
+        if (x <= 0 || x >= 100)
+            throw new IllegalArgumentException("number invalid" +x);
+        if (y <= 0 || y >= 100)
+            throw new IllegalArgumentException("number invalid" +y);
+
+        return x + y;
     }
 
 }
