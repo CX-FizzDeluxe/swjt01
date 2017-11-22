631591c5206466cf1a47e8fcc52c8dbf86964a38
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-22 19:58:43 +0000

    Wed Nov 22 19:58:43 GMT 2017

--- src/test/java/befaster/solutions/SumTest.java
+++ src/test/java/befaster/solutions/SumTest.java
@@ -11,4 +11,24 @@ public class SumTest {
     public void compute_sum() {
         assertThat(Sum.sum(1, 1), equalTo(2));
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidNumbersParam1LesserThanZero() {
+        Sum.sum(-1, 10);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidNumbersParam2LesserThanZero() {
+        Sum.sum(10, -1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidNumbersParam1GreaterThanOneHundred() {
+        Sum.sum(101, 10);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testInvalidNumbersParam2GreaterThanOneHundred() {
+        Sum.sum(10, 101);
+    }
 }
