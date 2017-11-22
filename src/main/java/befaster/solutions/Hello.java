895f3ee13a800fee9b27f34214b88e6885bfbffe
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-22 20:30:05 +0000

    Wed Nov 22 20:30:05 GMT 2017

--- src/main/java/befaster/solutions/Hello.java
+++ src/main/java/befaster/solutions/Hello.java
@@ -6,7 +6,7 @@ import java.text.MessageFormat;
 
 public class Hello {
     public static String hello(String friendName) {
-        if (friendName != null && "".equals(friendName)) {
+        if (friendName != null && !"".equals(friendName)) {
             return MessageFormat.format("Hello, {0}!", friendName);
         }
         return null;
