71d52b7bb1db15beef79cd4a18650bf7cab00cae
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2017-11-22 20:28:26 +0000

    Wed Nov 22 20:28:26 GMT 2017

--- src/main/java/befaster/solutions/Hello.java
+++ src/main/java/befaster/solutions/Hello.java
@@ -2,8 +2,13 @@ package befaster.solutions;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.text.MessageFormat;
+
 public class Hello {
     public static String hello(String friendName) {
-        return "Hello, World!";
+        if (friendName != null && "".equals(friendName)) {
+            return MessageFormat.format("Hello, {0}!", friendName);
+        }
+        return null;
     }
 }
